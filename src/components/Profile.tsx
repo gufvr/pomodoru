7bf30aa766d16ca2ec18b7817fa4230acc6f2443
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-03-21 00:38:29 -0300

    Criando os componentes CompletedChallenges.tsx, Profile.tsx, Countdowm.tsx e seus estilos com module.css

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={ styles.profileContainer }>
+      <img src="https://github.com/Gufvr.png" alt="Gustavo Favero" />
+      <div>
+        <strong>Gustavo Favero</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,21 +2,29 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
-    return(
+    return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-          rel="stylesheet"
-        />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&family=Roboto+Mono:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
-          <NextScript/>
+          <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
