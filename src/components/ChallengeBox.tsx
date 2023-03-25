d3f28e5cb341f6431b1fb5cee803e19e029b28e5
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-03-25 01:10:23 -0300

    Finalizando o botão de contagem de ciclos e seus useEffects e começando a desenvolver o container de desafios.

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
+
+export function ChallengeBox() {
+  return (
+    <div className={styles.challengeBoxContainer}>
+      <div className={styles.challengeNotActive}>
+        <strong>Finalize um ciclo para receber um desafio</strong>
+        <p>
+          <img src="icons/level-up.svg" alt="Level Up" />
+          Avance de level completando os desafios.
+        </p>
+      </div>
+    </div>
+  )
+}
