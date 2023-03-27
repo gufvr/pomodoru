47c8b5fd167fab3dc7c71c6aeb30e8a153dde91d
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-03-26 22:48:35 -0300

    Entendendo e utilizando a API de contexto para conectar as funcionalidades dos componentes de desafios.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { AppProps  } from 'next/app';
+
 import '../styles/global.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <ChallengesProvider>
+        <Component {...pageProps} />
+      </ChallengesProvider>
+  )
 }
 
 export default MyApp
