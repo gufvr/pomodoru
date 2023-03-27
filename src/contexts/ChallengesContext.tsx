f61cd8073cd330fe99180abadeaccc13a3eaace6
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-03-27 00:16:20 -0300

    Utilizando o hook "useEffect" para gerar os eventos de progressão de xp e iniciando as funcionalidades de botões do compontente de desafios.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,22 @@
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
+  experienceToNextLevel: number;
   challengesCompleted: number;
+  activeChallenge: Challenge | null;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
+  
 }
 
 interface ChallengesProviderProps{
@@ -16,24 +27,39 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExpecience] = useState(0);
+  const [currentExperience, setCurrentExpecience] = useState(32);
   const [challengesCompleted, setChellengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New Challenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    // @ts-ignore
+    setActiveChallenge(challenge);
   }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
   return (
     <ChallengesContext.Provider value={{
       level, 
-      currentExperience, 
+      currentExperience,
+      experienceToNextLevel,
       challengesCompleted, 
       levelUp,
       startNewChallenge,
+      activeChallenge,
+      resetChallenge,
     }}
     >
       { children }
